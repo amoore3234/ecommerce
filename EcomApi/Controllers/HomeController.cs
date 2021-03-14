e439ad057c91140bdb0324f4a372669a051a0c4a
Michelle Lee <michellechulee@gmail.com>
2021-03-13 23:04:22 -0800

    Revert "displayed data and notes"
    This reverts commit ee924a6060b879aaa3b555edab8fa3915f8fd811.

--- EcomApi/Controllers/HomeController.cs
+++ EcomApi/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using EcomApi.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,22 +6,12 @@ using System.Web.Mvc;
 
 namespace EcomApi.Controllers
 {
-
     public class HomeController : Controller
     {
         // GET: Home
         public ActionResult Index()
         {
-            ProductsController products = new ProductsController();
-
-            // ViewBag enables sharing of values dynamically between the Controller and View
-            ViewBag.Name = "JohnDoe";
-            ViewBag.Product1 = products.Get(1);
-            // ViewBag.Products = products.Get();
-
-            // View() returns the name of the action method which is the index, "ActionResult Index()"
             return View();
         }
-
     }
 }
